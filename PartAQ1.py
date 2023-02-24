f732abee5619426ffc0a4e79de14016399060689
hunterdih <hunter.davi@husky.neu.edu>
2023-02-24 16:40:46 -0500

    Problem 1 Complete

--- PartAQ1.py
+++ PartAQ1.py
@@ -3,13 +3,17 @@ import numpy as np
 
 
 if __name__ == '__main__':
-    outfile_path = r'C:\Users\David Hunter\OneDrive\Northeastern Classes\Graduate\Computer Architecture\Homework4\PartA'
+    outfile_path = r'C:\Users\dihdd\OneDrive\Northeastern Classes\Graduate\Computer Architecture\Homework4\PartA'
     block_size = 64
     cache_size = 8 #KB
-
+    offset = 64
     cache_size = 8*1024
-    file = open(outfile_path + '/' + 'trace1.txt', "a+")
-    for instruction in range(0,cache_size, block_size):
-        file.write(f'2 {instruction:x}\n')
+    file = open(outfile_path + '/' + 'trace1.din', "a+")
+    write_list = []
+    for instruction in range(offset,cache_size+offset, block_size):
+        file.write(str(f"2 {hex(instruction)}    \n"))
+
+    for instruction in range(offset,cache_size+offset, block_size):
+        file.write(str(f"2 {hex(instruction)}    \n"))
 
     file.close()
