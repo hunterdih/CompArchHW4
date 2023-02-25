f7cda2911202da4f323b7e8acd695aca82a8d556
David Hunter <hunter.davi@northeastern.edu>
2023-02-24 22:18:19 -0500

    Initial Commit

--- PartAQ1.py
+++ PartAQ1.py
@@ -3,16 +3,13 @@ import numpy as np
 
 
 if __name__ == '__main__':
-    outfile_path = r'C:\Users\dihdd\OneDrive\Northeastern Classes\Graduate\Computer Architecture\Homework4\PartA'
+    outfile_path = r'C:\Users\David Hunter\OneDrive\Northeastern Classes\Graduate\Computer Architecture\Homework4\PartA'
     block_size = 64
     cache_size = 8 #KB
     offset = 64
     cache_size = 8*1024
     file = open(outfile_path + '/' + 'trace1.din', "a+")
     write_list = []
-    for instruction in range(offset,cache_size+offset, block_size):
-        file.write(str(f"2 {hex(instruction)}    \n"))
-
     for instruction in range(offset,cache_size+offset, block_size):
         file.write(str(f"2 {hex(instruction)}    \n"))
 
--- PartAQ2.py
+++ PartAQ2.py
@@ -3,21 +3,27 @@ import numpy as np
 
 
 if __name__ == '__main__':
-    outfile_path = r'C:\Users\dihdd\OneDrive\Northeastern Classes\Graduate\Computer Architecture\Homework4\PartA'
-    block_size = 64
-    cache_size = 8 #KB
-    cache_size = 8*1024
-    loops = 3
+    outfile_path = r'C:\Users\David Hunter\OneDrive\Northeastern Classes\Graduate\Computer Architecture\Homework4\PartA'
+    block_size = 64 # Bytes
+    cache_size = 8 # KBytes
+    cache_size = 8*1024 # Bytes
     associativity = 4
     file = open(outfile_path + '/' + 'trace2.din', "a+")
 
-    for offset in range(0, loops):
-        for instruction in range(0 + associativity * (offset * block_size),
-                                 cache_size+associativity*(offset*block_size),
-                                 block_size*associativity):
+    loops = (cache_size/block_size)/associativity
 
-            file.write(str(f"2 {hex(instruction)}    \n"))
+    for instruction in range(0, cache_size, block_size):
+        file.write(f'2 {hex(instruction)}    \n') # Miss
+        file.write(f'2 {hex(instruction)}    \n') # Hit
 
-            file.write(str(f"2 {hex(instruction)}    \n"))
+    for instruction in range(cache_size, 2*cache_size, block_size):
+        file.write(f'2 {hex(instruction)}    \n') # Miss
+        file.write(f'2 {hex(instruction)}    \n') # Hit
+    for instruction in range(cache_size, 2*cache_size, block_size):
+        file.write(f'2 {hex(instruction)}    \n') # Miss
+        file.write(f'2 {hex(instruction)}    \n') # Hit
 
+        # 3 Memory address values per line used, revert to first memory address
+    for instruction in range(0, cache_size, block_size):
+        file.write(f'2 {hex(instruction)}    \n') # Miss
     file.close()
