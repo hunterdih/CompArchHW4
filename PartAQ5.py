108a90d8c59f0507db9a86a097916fddab3ed054
David Hunter <hunter.davi@northeastern.edu>
2023-02-26 12:08:39 -0500

    Initial Commit

--- PartAQ5.py
+++ PartAQ5.py
@@ -2,37 +2,14 @@ if __name__ == '__main__':
     outfile_path = r'C:\Users\David Hunter\OneDrive\Northeastern Classes\Graduate\Computer Architecture\Homework4\PartA'
     block_size = 16  # Bytes
     cache_size = 32  # KBytes
-    cache_size_bytes = 1024  # Bytes
-    associativity = 4
+    cache_size_bytes =cache_size* 1024  # Bytes
+    max_associativity = 4
     file = open(outfile_path + '/' + 'trace5.din', "a+")
-
-    for instruction in range(0, 1024, block_size):
-        file.write(f'2 {hex(instruction)}    \n')
-    for instruction in range(0, 1024, block_size):
-        file.write(f'2 {hex(instruction)}    \n')
-
-    for instruction in range(1024, 3072, block_size):
-        file.write(f'2 {hex(instruction)}    \n')
-    for instruction in range(1024, 3072, block_size):
-        file.write(f'2 {hex(instruction)}    \n')
-
-    for instruction in range(3072, 7168, block_size):
-        file.write(f'2 {hex(instruction)}    \n')
-    for instruction in range(3072, 7168, block_size):
-        file.write(f'2 {hex(instruction)}    \n')
-
-    for instruction in range((4 * 1024), (8 * 1024) + (4 * 1024), block_size):
-        file.write(f'2 {hex(instruction)}    \n')
-    for instruction in range((4 * 1024), (8 * 1024) + (4 * 1024), block_size):
-        file.write(f'2 {hex(instruction)}    \n')
-
-    for instruction in range((8 * 1024), (16 * 1024) + (8 * 1024), block_size):
-        file.write(f'2 {hex(instruction)}    \n')
-    for instruction in range((8 * 1024), (16 * 1024) + (8 * 1024), block_size):
-        file.write(f'2 {hex(instruction)}    \n')
-
-    for instruction in range((16 * 1024), (32 * 1024) + (16 * 1024), block_size):
-        file.write(f'2 {hex(instruction)}    \n')
-    for instruction in range((16 * 1024), (32 * 1024) + (16 * 1024), block_size):
-        file.write(f'2 {hex(instruction)}    \n')
+    address_list = []
+    for address in range(0, max_associativity*cache_size_bytes, cache_size_bytes):
+        address_list.append(f'2 {address:x}  \n')
+    file.writelines(address_list)
+    reversed_address_list = address_list[::-1]
+    file.writelines(reversed_address_list)
     file.close()
+
